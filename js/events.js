e629891284cb7e8f72543e0ae283513bb533082e
deepesh yadav <yadavdeepesh13@gmail.com>
2023-08-25 18:33:34 +0530

    Events in Js Some event are use

---
+++ js/events.js
@@ -0,0 +1,12 @@
+function helloWorld(){
+
+    alert(" Welcome to My kingdom Deepesh ");
+}
+
+function keyValue(){
+    alert("User Enter the value ");
+}
+
+function pageLoad(){
+    alert("page is loaded");
+}
