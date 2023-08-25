db9d129a166238e094175acb81a47e0d31ec572f
deepesh yadav <yadavdeepesh13@gmail.com>
2023-08-25 13:42:40 +0530

    parameterized function and default parameterized function

--- js/function.js
+++ js/function.js
@@ -15,8 +15,19 @@ helloWorld();
 helloWorld();
 */
 
-for(i=1;i<=100;i++){
-    helloWorld();
-    document.write(i);
+// for(i=1;i<=100;i++){
+//     helloWorld();
+//     document.write(i);
+//     document.write("<br>");
+// }
+
+// parameterized function
+// default value
+function firstLastName(first="Tushar",last="Yadav"){
+    document.write("hello "+first+" "+last);
     document.write("<br>");
 }
+
+firstLastName("Deepesh","Yadav");
+firstLastName("khushi","Yadav");
+firstLastName();
