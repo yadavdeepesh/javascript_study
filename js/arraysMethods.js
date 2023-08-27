c89635fb515b65d91329e71acffac9e3e742220a
deepesh yadav <yadavdeepesh13@gmail.com>
2023-08-27 16:39:49 +0530

    array method pop and push

---
+++ js/arraysMethods.js
@@ -0,0 +1,27 @@
+// Arrays Methods
+var arr = ["ravi ","shubham","Ashish" , "Ramesh"];
+var numArr = [66,98,22,23,20,49];
+// sort array element
+
+document.write("<br> After Sort String , Number Array"+"<br>");
+
+document.write("<br> String Array " + arr + "<br>");
+
+document.write("<br> Number Array " + numArr + "<br>");
+
+document.write("<br> After Sort String , Number Array"+"<br>");
+
+document.write("<br>Sort String Array " + arr.sort() + "<br>");
+
+document.write("<br> Sort Number Array " + numArr.sort() + "<br>");
+
+document.write("<br>Reverse String Array " + arr.sort() + "<br>");
+
+document.write("<br>Reverse Number Array " + numArr.sort() + "<br>");
+
+
+document.write("<br>Pop Array " + numArr.pop() + "<br>");
+
+document.write("<br>Push Array " + numArr.pop(55) + "<br>");
+
+document.write("<br>Pop Array " + numArr + "<br>");
